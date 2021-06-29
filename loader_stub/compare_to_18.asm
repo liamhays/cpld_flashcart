99f98c73c82a79377937ab8f92a061b7461e32c9
Liam Hays <liamrhays@gmail.com>
2021-06-29 14:26:34 -0600

    Added gg-card-dimensions.svg, an Inkscape SVG of a GG cartridge PCB.

--- loader_stub/compare_to_18.asm
+++ loader_stub/compare_to_18.asm
@@ -2,15 +2,11 @@
 ; This subroutine does exactly what it says. The argument
 ; is in a, and the Boolean result is returned in a.
 LessThanOrEqualTo18:
-	; if the length of the table is less than or equal to 18, we
-	; don't want to do anything with an offset
-	;ld a, b
 	sub 18
 	; subtraction is actually signed, oddly, so we can check
 	; bit 7 of a because it will be 1 if the table length is  18.
-	bit 7, a
 	; if less than (sign bit is 1), return 1
-	jr nz, Exit1
+	jp m, Exit1
 	; if equal to (result is 0), also return 1
 	; we can just check the result of the subtraction, which is quick
 	; and easy
